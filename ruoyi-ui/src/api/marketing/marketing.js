004fa92f339e5b9e41a1dbe151f6122ff9dd88d3
Tim Xu <908993226@qq.com>
2023-11-26 20:14:03 +0400

    添加活动api

--- ruoyi-ui/src/api/marketing/marketing.js
+++ ruoyi-ui/src/api/marketing/marketing.js
@@ -10,6 +10,24 @@ export function addActivity(param) {
   })
 }
 
+// 添加活动
+export function queryActivityList(param) {
+  return request({
+    url: '/activity/list',
+    method: 'post',
+    params: param
+  })
+}
+
+// 添加活动
+export function queryActivity(param) {
+  return request({
+    url: '/activity/detail',
+    method: 'post',
+    params: param
+  })
+}
+
 
 // 添加场景
 export function addScene(param) {
